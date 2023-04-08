eb597f079013d0a0b791cfa0862bf44c6769f2f9
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-08 06:13:28 -0300

    Classes
    Criado um exemplo de classes

---
+++ Classes/Models/Gerente.cs
@@ -0,0 +1,10 @@
+namespace Gerente{
+    class Gerente{
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public void AprovarHoras()
+        {
+            //Construir função lógica
+        }
+    }
+}
