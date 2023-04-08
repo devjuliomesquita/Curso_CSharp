2590983e7c066b7af268519b7c858288d0c34ca6
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-08 05:35:15 -0300

    Namespaces
    Criado o código de explicação de namespaces

---
+++ Namespace/Models/ClasseTres.cs
@@ -0,0 +1,6 @@
+namespace Programa1{
+    class ClasseTres{
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ Namespace/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Programa2;
+namespace ProgramaPrincipal{
+    //ClasseUm já está dentro do namespace Programa1
+    class ClasseUm{
+        public static void Main(string[] args)
+        {
+            //Chamando a classe de outro namespace por conta do using no início do código.
+            ClasseDois classeDois = new ClasseDois();
+            //Chamando a classe de outro namespace de um jeito mais verboso trazendo todo o caminho até a classe.
+            Programa1.ClasseTres classeTres = new Programa1.ClasseTres();
+            
+        }
+    }
+}
