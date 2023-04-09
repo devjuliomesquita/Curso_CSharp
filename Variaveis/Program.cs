73cd3e1b045beab52d7543773c9581904dba227e
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-04-09 01:45:49 -0300

    variáveis

---
+++ Variaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Program{
+    class Test{
+        public void Method()
+        {
+            //Criando uma variável com a palavra reservada var
+            var variavel = "Variável";
+            //Tipos integrais
+            ///Inteiro - todos os números naturais
+            int num = 1;
+            ///Long - números muito grandes que não cabem dentro do inteiro e podem variar de sinal
+            long num2 = 9223372036854775807;
+            ///Char - Apenas um caractere
+            char carac = 'A';
+            //Tipos flutuantes - São números flutuantes que aceitam aproximação por vírgula
+            ///Float - São números com 64 bits
+            float num3;
+            ///Double - Ponto com precisão dupla de 64 bits
+            double num4;
+            //TIpos boleanos - Verdadeiro ou falso.
+            bool teste = true;
+            bool teste2 = false;
+            //Variável do tipo nulo
+            var teste3 = null;
+        }
+    }
+}
